3c77b90831e160f1921aa844e75e9f6f7566a826
Darya Razumova <d.razumova@yandex.ru>
2019-02-01 23:15:40 +0300

    node added

--- Trie/src/main/java/com/hw/trie/Trie.java
+++ Trie/src/main/java/com/hw/trie/Trie.java
@@ -33,15 +33,61 @@ public class Trie implements Serialize{
             edges = new TrieNode[listLenght];
         }
 
+        private TrieNode goByLetter(char character) {
+            stringsCounter++;
+            if (edges[character] == null) {
+                edges[character] = new TrieNode();
+            }
+            return edges[character];
+        }
+
 
         @Override
         public void serialize(OutputStream out) throws IOException {
+            out.write(stringsCounter);
+            out.write(endsCounter);
 
+            for (int i = 0; i < edges.length; i++) {
+                if (edges[i] != null) {
+                    out.write(1);
+                } else {
+                    out.write(0);
+                }
+            }
         }
 
         @Override
         public void deserialize(InputStream in) throws IOException {
+            stringsCounter = in.read();
+            endsCounter = in.read();
 
+            for (int i = 0; i < edges.length; i++) {
+                int currentEdge = in.read();
+                if (currentEdge == 1) {
+                    edges[i] = this;
+                }
+            }
+        }
+
+    }
+
+    private TrieNode deserializeTree(InputStream in) throws IOException {
+        var node = new TrieNode();
+        node.deserialize(in);
+        for (int i = 0; i < node.edges.length; i++) {
+            if (node.edges[i] != null) {
+                node.edges[i] = deserializeTree(in);
+            }
+        }
+        return node;
+    }
+
+    private void serializeTree(TrieNode node, OutputStream out) throws IOException {
+        node.serialize(out);
+        for (int i = 0; i < node.edges.length; i++) {
+            if (node.edges[i] != null) {
+                serializeTree(node.edges[i], out);
+            }
         }
     }
 
