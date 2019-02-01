afd75193c34ecd7574301b8dd2df0bf7fcf0adbe
Darya Razumova <d.razumova@yandex.ru>
2019-02-01 23:24:11 +0300

    trie methods added

--- Trie/src/main/java/com/hw/trie/Trie.java
+++ Trie/src/main/java/com/hw/trie/Trie.java
@@ -12,21 +12,14 @@ interface Serialize{
  */
 public class Trie implements Serialize{
 
-    @Override
-    public void serialize(OutputStream out) throws IOException {
-
-    }
-
-    @Override
-    public void deserialize(InputStream in) throws IOException {
-
-    }
+    private TrieNode root;
 
     private class TrieNode implements Serialize{
         private TrieNode[] edges;
         private int stringsCounter;
         private int endsCounter;
         private static final int listLenght = Character.MAX_VALUE + 1;
+
 
 
         private TrieNode() {
@@ -91,11 +84,106 @@ public class Trie implements Serialize{
         }
     }
 
-    private TrieNode root;
 
 
     public Trie() {
         root = new TrieNode();
+    }
+
+    /**
+     * Write trie to a stream
+     * @param out the stream to write
+     * @throws IOException
+     */
+    @Override
+    public void serialize(OutputStream out) throws IOException {
+        serializeTree(root, out);
+    }
+
+    /**
+     * Reads trie from a stream
+     * @param in the stream from which trie will be read
+     * @throws IOException
+     */
+    @Override
+    public void deserialize(InputStream in) throws IOException {
+        root = deserializeTree(in);
+    }
+
+    private TrieNode getNodeByString(String string) {
+        TrieNode current = root;
+        var characterList = string.toCharArray();
+
+        for (int i = 0; i < string.length() && current != null; i++) {
+            current = current.edges[characterList[i]];
+        }
+
+        return current;
+
+    }
+
+    /**
+     * Adding element to trie
+     * @return is it a new element for the trie
+     */
+    boolean add(String element) {
+        TrieNode current = root;
+        boolean result = contains(element);
+        var characterList = element.toCharArray();
+
+        for (int i = 0; i < element.length(); i++) {
+            current = current.goByLetter(characterList[i]);
+        }
+        current.stringsCounter++;
+        current.endsCounter++;
+        return result;
+    }
+
+    /**
+     * Tells does trie contain such element
+     */
+    boolean contains(String element) {
+        TrieNode node = getNodeByString(element);
+        return (node != null && node.endsCounter > 0);
+    }
+
+    /**
+     * Removes element from the trie
+     * @return did trie contain this element
+     */
+    boolean remove(String element) {
+        if (!contains(element)) {
+            return false;
+        }
+
+        TrieNode current = root;
+        var characterList = element.toCharArray();
+
+        for(int i = 0; i < element.length(); i++) {
+            current.stringsCounter--;
+            current = current.edges[characterList[i]];
+        }
+        current.stringsCounter--;
+        current.endsCounter--;
+        return true;
+    }
+
+    /**
+     * Returns total strings in trie
+     */
+    int size() {
+        return root.stringsCounter;
+    }
+
+    /**
+     * Returns count of strings in trie that have such prefix
+     */
+    int howManyStartsWithPrefix(String prefix) {
+        TrieNode node = getNodeByString(prefix);
+        if (node == null) {
+            return 0;
+        }
+        return node.stringsCounter;
     }
 
 }
