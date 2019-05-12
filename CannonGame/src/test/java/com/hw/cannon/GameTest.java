e228854c1dcd8cb9e5506af619eb8b2085eaddf2
Darya Razumova <d.razumova@yandex.ru>
2019-05-12 18:44:08 +0300

    tests added

---
+++ CannonGame/src/test/java/com/hw/cannon/GameTest.java
@@ -0,0 +1,77 @@
+package com.hw.cannon;
+
+import javafx.scene.paint.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void simpleIsCloseTest() {
+        var firstBomb = new Bomb(0, 0, 10, Color.RED, 1, 1);
+        var secondBomb = new Bomb(1, 1, 20, Color.BLACK, 1, 1);
+        assertTrue(firstBomb.isClose(secondBomb));
+        assertTrue(secondBomb.isClose(firstBomb));
+    }
+
+    @Test
+    void isCloseInOneTest() {
+        var firstBomb = new Bomb(0, 0, 1, Color.RED, 1, 1);
+        var secondBomb = new Bomb(1.1, 1.1, 20, Color.BLACK, 1, 1);
+        assertFalse(firstBomb.isClose(secondBomb));
+        assertFalse(secondBomb.isClose(firstBomb));
+    }
+
+    @Test
+    void isCloseOutOfRadiusTest() {
+        var firstBomb = new Bomb(0, 0, 10, Color.RED, 1, 1);
+        var secondBomb = new Bomb(100, 100, 20, Color.BLACK, 1, 1);
+        assertFalse(firstBomb.isClose(secondBomb));
+        assertFalse(secondBomb.isClose(firstBomb));
+    }
+
+    @Test
+    void simpleCorYTest() {
+        var xs = new Double[] {0.0, 1.0, 2.0};
+        var ys = new Double[] {0.0, 1.0, 0.0};
+        var mount = new Mount(xs, ys);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(mount.getYCor(xs[i]), ys[i]);
+        }
+    }
+
+    @Test
+    void complexCorYTest() {
+        var xs = new Double[] {0.0, 2.0};
+        var ys = new Double[] {0.0, 0.0};
+        var mount = new Mount(xs, ys);
+        assertEquals(Double.valueOf(0.0), mount.getYCor(1.0));
+    }
+
+    @Test
+    void simpleIsUnderTest() {
+        var xs = new Double[] {0.0, 1.0, 2.0};
+        var ys = new Double[] {0.0, 1.0, 0.0};
+        var mount = new Mount(xs, ys);
+        for (int i = 0; i < 3; i++) {
+            assertFalse(mount.isUnder(xs[i], ys[i]));
+        }
+    }
+
+    @Test
+    void underIsUnderTest() {
+        var xs = new Double[] {0.0, 1.0, 2.0};
+        var ys = new Double[] {0.0, 1.0, 0.0};
+        var mount = new Mount(xs, ys);
+        assertTrue(mount.isUnder(1.5, 0.0));
+    }
+
+    @Test
+    void aboveIsUnderTest() {
+        var xs = new Double[] {0.0, 1.0, 2.0};
+        var ys = new Double[] {0.0, 1.0, 0.0};
+        var mount = new Mount(xs, ys);
+        assertFalse(mount.isUnder(0.5, 1.0));
+    }
+}
